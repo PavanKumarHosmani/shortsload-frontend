712183c569d208127756e31fd107b235845b8a26
pavanhosmani <PavanHosmani>
2025-11-27 00:46:42 +0530

    fixed redirect issues

--- next.config.mjs
+++ next.config.mjs
@@ -16,14 +16,6 @@ const nextConfig = {
 
   async redirects() {
     return [
-      // ✅ ONLY redirect non-WWW to WWW
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'fbvideodownloaderonline.com' }],
-        destination: 'https://www.fbvideodownloaderonline.com/:path*',
-        permanent: true,
-      },
-
       // ✅ Clean index URLs
       { source: '/index.html', destination: '/', permanent: true },
       { source: '/index.php', destination: '/', permanent: true },
